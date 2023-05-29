01843e90393de24eb04fc11b3edd13569876f311
Evanlab02 <evanlabuschagne70@gmail.com>
2023-05-29 15:49:35 +0200

    Project-Setup

---
+++ src/pages/Overview.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DoughnutGraph from "../components/DoughnutGraph";
+
+export default function Overview() {
+    return (
+        <div>
+            <h1>Lyzer</h1>
+            <h2>Overview</h2>
+            <DoughnutGraph />
+        </div>
+    );
+}
